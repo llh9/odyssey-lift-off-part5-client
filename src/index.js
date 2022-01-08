9339cba3dcf62145461fdee1bef2bc33873cfb69
Montii <llh9@yahoo.com>
2022-01-07 19:19:13 -0500

    add uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://catstronauts-on-lift.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
